c371ca9a4f4deffad13775b4fa7b60c981f84b99
sergeykrasilnikov1 <100011841+sergeykrasilnikov1@users.noreply.github.com>
2024-12-21 16:07:56 +0700

    Add files via upload

---
+++ autorace_2023/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+package_name = 'autorace_2023'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'images', 'templates'), glob(os.path.join('images', 'templates', '*.png'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ruby',
+    maintainer_email='dimaskrut71@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "detect = autorace_2023.detect:main",
+            "detect_lane = autorace_2023.detect_lane:main",
+            "pid_lane = autorace_2023.pid_lane:main",
+        ],
+    },
+)
